963c515ebe4917b902f8d41351b906b67b345f8f
Oliver Evans <oliverevans96@gmail.com>
2021-01-22 03:31:02 -0500

    Move main.rs -> bin/hub-server.rs: fixes dead code & import warnings

--- src/bin/hub-server.rs
+++ src/bin/hub-server.rs
@@ -1,28 +1,28 @@
 use async_std::channel;
 use std::error::Error;
+use std::sync::mpsc;
 use tokio::try_join;
 
-mod controller;
-mod grpc;
-mod utils;
-mod vrpn;
-
-use controller::{Controller, ControllerConfig};
-use grpc::proto::Pose3D;
-use grpc::server::{LabeledPose2D, NamedCameraInfo};
+use posenet_vr_hub::controller::{Controller, ControllerConfig};
+use posenet_vr_hub::grpc::proto::Pose3D;
+use posenet_vr_hub::grpc::server::{LabeledPose2D, NamedCameraInfo};
+use posenet_vr_hub::{grpc, vrpn};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let (cameras_tx, cameras_rx) = channel::unbounded::<NamedCameraInfo>();
     let (poses2d_tx, poses2d_rx) = channel::unbounded::<LabeledPose2D>();
-    let (poses3d_tx, poses3d_rx) = channel::unbounded::<Pose3D>();
+    let (poses3d_tx, poses3d_rx) = mpsc::channel::<Pose3D>();
     let config = ControllerConfig::default();
-    let mut controller = Controller::new(cameras_rx, poses2d_rx, poses3d_tx, config);
+    let controller = Controller::new(cameras_rx, poses2d_rx, poses3d_tx, config);
+    println!("Hub main start");
+
     try_join!(
         controller.run(),
         grpc::server::main(cameras_tx, poses2d_tx),
         vrpn::server::main(poses3d_rx),
     )?;
+    println!("Hub main end");
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod controller;
 pub mod grpc;
 pub mod openmvg;
 mod utils;
