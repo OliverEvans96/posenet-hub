875cca63d2241e47e056cf593c04ea45a3521f8c
Oliver Evans <oliverevans96@gmail.com>
2021-01-23 22:25:50 -0500

    Fix default grpc client port

--- src/bin/grpc-client.rs
+++ src/bin/grpc-client.rs
@@ -3,7 +3,7 @@ use posenet_vr_hub::grpc::proto::hub_service_client::HubServiceClient;
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "http://[::1]:50051";
+    let addr = "http://127.0.0.1:50051";
     println!("Connecting");
     let mut client = HubServiceClient::connect(addr).await?;
     println!("Sending hello");
