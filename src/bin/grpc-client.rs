e6f74eeecb810fdee160815eb5b675e8bf7cc60b
Oliver Evans <oliverevans96@gmail.com>
2021-09-28 12:13:47 -0700

    Start using structopt and log in grpc-client

--- src/bin/grpc-client.rs
+++ src/bin/grpc-client.rs
@@ -1,35 +1,81 @@
-use clap::App;
+use structopt::StructOpt;
 
-use posenet_vr_hub::grpc::client::{hello, stream_poses};
+use posenet_vr_hub::grpc::client as grpc_client;
 use posenet_vr_hub::grpc::proto::hub_service_client::HubServiceClient;
+
+#[derive(StructOpt)]
+struct CommonOpts {
+    /// gRPC Port
+    #[structopt(short, long, default_value = "50051")]
+    port: u16,
+    /// Server address
+    #[structopt(short, long, default_value = "localhost")]
+    server: String,
+}
+
+#[derive(StructOpt)]
+#[structopt(
+    name = "grpc-client",
+    version = "0.1.0",
+    about = "gRPC client to connect to posenet-hub server"
+)]
+enum GrpcClientOpts {
+    /// Act as a camera, streaming randomly generated poses to the server
+    Stream {
+        /// Group name for client
+        #[structopt(short, long, default_value = "grpc_client")]
+        group: String,
+        #[structopt(flatten)]
+        common: CommonOpts,
+    },
+    /// Act as a non-camera client, and request a snapshot
+    Snapshot {
+        /// Group name for client
+        #[structopt(short, long)]
+        group: String,
+        #[structopt(flatten)]
+        common: CommonOpts,
+    },
+    // TODO: non-camera snapshot client
+}
+
+impl GrpcClientOpts {
+    /// Any variant should have the common options, so provide a unified interface
+    fn get_common(&self) -> &CommonOpts {
+        match self {
+            GrpcClientOpts::Stream { common, .. } => common,
+            GrpcClientOpts::Snapshot { common, .. } => common,
+        }
+    }
+}
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let matches = App::new("grpc-client")
-        .version("0.1.0")
-        .about("Test grpc-client sends random 2D poses to posenet hub-server.")
-        .args_from_usage(
-            "-s, --server=[server] 'Server address (default: localhost)'
-                            -p, --port=[port]      'Grpc port (default: 50051)'
-                            -g, --group=[group]    'Client group name (default: grpc_client)'",
-        )
-        .get_matches();
+    dotenv::dotenv().ok();
+    env_logger::init();
 
-    // Get config flags or defaults
-    let server = matches.value_of("server").unwrap_or("127.0.0.1");
-    let port = matches.value_of("port").unwrap_or("50051");
-    let group = matches.value_of("group").unwrap_or("grpc_client");
+    let opts = GrpcClientOpts::from_args();
+    let common_opts = opts.get_common();
 
-    // TODO: subcommands for streaming vs snapshot?
-
-    let addr = format!("http://{}:{}", server, port);
-    println!("Connecting");
+    let addr = format!("http://{}:{}", common_opts.server, common_opts.port);
+    log::info!("Connecting to server at '{}'", addr);
     let mut client = HubServiceClient::connect(addr).await?;
-    println!("Sending hello");
-    let name = hello(&mut client, group).await?;
-    println!("Streaming poses");
-    stream_poses(&mut client, group, &name).await?;
-    println!("Done");
+
+    match opts {
+        GrpcClientOpts::Stream { group, .. } => {
+            log::info!("Sending hello");
+            let name = grpc_client::hello(&mut client, &group).await?;
+            log::info!("Streaming poses");
+            grpc_client::stream_poses(&mut client, &group, &name).await?;
+        }
+        GrpcClientOpts::Snapshot { group, .. } =>{
+          log::info!("Waiting for snapshot request");
+          // TODO: This subcommand is actually the non-camera version
+          grpc_client::wait_for_snapshot_request(&mut client, group).await?;
+        },
+    };
+
+    log::info!("Done");
 
     Ok(())
 }
